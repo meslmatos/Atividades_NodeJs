214500ced771527fa44a7e2631f7a2d1d0893450
meimoon1 <melevelyn69@gmail.com>
2021-12-01 15:00:58 -0300

    atp69

---
+++ ATP69/variaveis.js
@@ -0,0 +1,12 @@
+const nome = "Melissa";
+const sobrenome = "Matos";
+const dataNascimento = "26/12/1997";
+const cpf = "0000000000";
+const rg = "222211111111";
+
+console.log(nome);
+console.log(sobrenome);
+console.log(dataNascimento);
+console.log(cpf);
+console.log(rg);
+console.log(nome,sobrenome,dataNascimento,cpf,rg);
