31feafd7c1a3acf09de025876a60f16732b72cff
meimoon1 <melevelyn69@gmail.com>
2021-12-06 09:52:35 -0300

    comits atrasados

---
+++ ATP73/banco/GerarDataBase.js
@@ -0,0 +1,7 @@
+import Database from "./Database.js";
+import ClienteTable from "./ClienteTable.js";
+
+Database
+    .sync()
+    .then(()=> console.log('Sincronizando database'))
+    .catch(()=> console.log('Deu erro'));
